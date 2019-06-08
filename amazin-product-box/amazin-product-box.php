e6d3ef6ba48ea7ce180012e75dae30499a7bd872
Mandi Grant <mandi018@gmail.com>
2019-06-08 10:24:33 -0500

    Initial commit

---
+++ amazin-product-box/amazin-product-box.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Amazin' Product Box
+ * Plugin URI: http://majoh.dev
+ * Description: Customizable product box for Amazon products with an affiliate link
+ * Version: 1.0
+ * Author: Mandi Grant
+ * Author URI: http://majoh.dev
+ */
